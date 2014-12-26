c73a2199e719874c8e1a7adf8b2bdafae4b77e60
Sebastian Staudt <koraktor@gmail.com>
2014-12-26 16:10:09 +0100

    Ignore errors from invalid requests in Airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -2,5 +2,13 @@ if ENV.include? 'AIRBRAKE_API_KEY'
   Airbrake.configure do |config|
     config.api_key = ENV['AIRBRAKE_API_KEY']
     config.ignore << ActionController::BadRequest
+    config.ignore_by_filter do |notice|
+      begin
+        route = Rails.application.routes.recognize_path notice.url
+        route[:action] == 'not_found' || route[:action] == 'forbidden'
+      rescue ActionController::RoutingError
+        true
+      end
+    end
   end
 end
