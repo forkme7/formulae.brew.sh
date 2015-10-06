30f17537c8bb75400b0f1e7b54275784f4b7436d
Sebastian Staudt <koraktor@gmail.com>
2015-10-06 07:20:31 +0200

    Sandbox MacOS::Xcode version methods

--- lib/sandbox_macos.rb
+++ lib/sandbox_macos.rb
@@ -1,7 +1,7 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2013-2014, Sebastian Staudt
+# Copyright (c) 2013-2015, Sebastian Staudt
 
 unless defined? ::MacOS
   ::MacOS = ::OS::Mac if defined? ::OS::Mac
@@ -11,5 +11,12 @@ if defined? ::MacOS
   MacOS.methods.select { |m| m.to_s =~ /_version$/ }.each do |m|
     MacOS.send :undef_method, m
     MacOS.send :define_method, m, ->{ '0' }
+  end
+
+  if defined? ::MacOS::Xcode
+    MacOS::Xcode.methods.select { |m| m.to_s =~ /_version$/ }.each do |m|
+      MacOS::Xcode.send :undef_method, m
+      MacOS::Xcode.send :define_method, m, ->{ '0' }
+    end
   end
 end
