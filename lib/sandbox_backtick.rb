ea02a1e44139c6fc85c07366ca27bcb7f98dd62b
Sebastian Staudt <koraktor@gmail.com>
2015-10-28 20:59:08 +0100

    Fix sandboxing for OS::Mac.version

--- lib/sandbox_backtick.rb
+++ lib/sandbox_backtick.rb
@@ -1,7 +1,7 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2012-2013, Sebastian Staudt
+# Copyright (c) 2012-2015, Sebastian Staudt
 
 module Kernel
 
@@ -12,8 +12,6 @@ module Kernel
       File.join $homebrew_path, 'bin', 'brew'
     elsif command == '/usr/bin/mdfind "kMDItemCFBundleIdentifier == \'com.apple.dt.Xcode\'"'
       '/Applications/Xcode.app'
-    elsif command =~ /sw_vers -productVersion$/
-      '10.9'
     elsif command == 'xcodebuild -version 2>&1'
       "Xcode 5.0.2\nBuild version 5A3005"
     elsif command == 'php -v'
--- lib/sandbox_macos.rb
+++ lib/sandbox_macos.rb
@@ -10,7 +10,11 @@ end
 if defined? ::MacOS
   MacOS.methods.select { |m| m.to_s =~ /_version$/ }.each do |m|
     MacOS.send :undef_method, m
-    MacOS.send :define_method, m, ->{ '1.0' }
+    if m == :full_version
+      MacOS.send :define_method, m, ->{ '10.11' }
+    else
+      MacOS.send :define_method, m, ->{ '1.0' }
+    end
   end
 
   if defined? ::MacOS::Xcode
