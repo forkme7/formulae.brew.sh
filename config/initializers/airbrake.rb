81a0f7c6ece8d1983fe34f20c49c1792d8175bcb
Sebastian Staudt <koraktor@gmail.com>
2016-05-10 08:09:39 +0200

    Update configuration for Airbrake 5

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,14 +1,21 @@
 if ENV.include? 'AIRBRAKE_API_KEY'
   Airbrake.configure do |config|
-    config.api_key = ENV['AIRBRAKE_API_KEY']
-    config.ignore << ActionController::BadRequest
-    config.ignore_by_filter do |notice|
-      begin
-        route = Rails.application.routes.recognize_path notice.url
-        route[:action] == 'not_found' || route[:action] == 'forbidden'
-      rescue ActionController::RoutingError
-        true
-      end
+    config.project_id = ENV['AIRBRAKE_PROJECT_ID']
+    config.project_key = ENV['AIRBRAKE_API_KEY']
+  end
+
+  Airbrake.add_filter do |notice|
+    if notice[:errors].any? { |error| error[:type] == 'ActionController::BadRequest' }
+      notice.ignore!
+    end
+  end
+
+  Airbrake.add_filter do |notice|
+    begin
+      route = Rails.application.routes.recognize_path notice.url
+      route[:action] == 'not_found' || route[:action] == 'forbidden'
+    rescue ActionController::RoutingError
+      notice.ignore!
     end
   end
 end
