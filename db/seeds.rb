c2ac028cc2d7357242b13e71d0038a94872357e0
Sebastian Staudt <koraktor@gmail.com>
2017-04-26 17:24:55 +0200

    Fix and improve db:seed
    This adapts the code to recent changes in the import logic. Instead of
    hard-coding official taps this also allows us to load them from brew.
    
    Closes #47

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,18 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2012-2016, Sebastian Staudt
+# Copyright (c) 2012-2017, Sebastian Staudt
 
-require 'repository_import'
+require 'main_import'
+require 'tap_import'
 
-Repository.include RepositoryImport
+main = Repository.find_or_initialize_by name: Repository::MAIN
+main.extend MainImport
+main.update_status
+main.create_missing_taps
 
-Repository.find_or_create_by name: 'Homebrew/brew'
-Repository.find_or_create_by name: 'Homebrew/homebrew-core'
-Repository.find_or_create_by name: 'Homebrew/homebrew-apache'
-Repository.find_or_create_by name: 'Homebrew/homebrew-binary'
-Repository.find_or_create_by name: 'Homebrew/homebrew-boneyard'
-Repository.find_or_create_by name: 'Homebrew/homebrew-completions'
-Repository.find_or_create_by name: 'Homebrew/homebrew-dupes'
-Repository.find_or_create_by name: 'Homebrew/homebrew-games'
-Repository.find_or_create_by name: 'Homebrew/homebrew-php'
-Repository.find_or_create_by name: 'Homebrew/homebrew-python'
-Repository.find_or_create_by name: 'Homebrew/homebrew-headonly'
-Repository.find_or_create_by name: 'Homebrew/homebrew-science'
-Repository.find_or_create_by name: 'Homebrew/homebrew-versions'
-
-Repository.all.each do |repo|
+(Repository.all - [ main ]).each do |repo|
+  repo.extend TapImport
   repo.refresh
   repo.recover_deleted_formulae
   repo.save!
